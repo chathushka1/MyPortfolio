77ff985a9d59db63022805c16c1df04647dbb6a5
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-06-16 12:51:51 +0530

    Responsive Laptop Size Done

---
+++ assignment/js/InMemory-POS/SPA/customer.js
@@ -0,0 +1,328 @@
+initiateUI();
+
+function initiateUI() {
+    clearAll();
+    $("#customerContent").css("display", "block");
+
+    setTheLastView();
+}
+
+function saveLastView(clickedID) {
+    switch (clickedID) {
+        case "customerContent":
+            localStorage.setItem("view", "CUSTOMER");
+            break;
+        case "itemContent":
+            localStorage.setItem("view", "ITEM");
+            break;
+        case "ordersContent":
+            localStorage.setItem("view", "ORDERS");
+            break;
+        case "placeOrdersContent":
+            localStorage.setItem("view", "PLACEORDERS");
+            break;
+    }
+}
+
+function setTheLastView() {
+    let view = localStorage.getItem("view");
+    switch (view) {
+        case "CUSTOMER":
+            setView($("#customerContent"));
+            break;
+        case "ITEM":
+            setView($("#itemContent"));
+            break;
+        case "ORDERS":
+            setView($("#ordersContent"));
+            break;
+        case "PLACEORDERS":
+            setView($("#placeOrdersContent"));
+            break;
+        default:
+            setView($("#customerContent"));
+    }
+}
+
+function clearAll() {
+    $("#customerContent,#itemContent,#ordersContent,#placeOrdersContent").css('display', 'none');
+}
+
+function setView(viewOb) {
+    clearAll();
+    viewOb.css("display", "block");
+    saveLastView(viewOb.get(0).id);
+    console.log(viewOb.get(0).id);
+}
+
+$("#customers").click(function () {
+    setView($("#customerContent"));
+});
+
+$("#items").click(function () {
+    setView($("#itemContent"));
+});
+
+$("#orders").click(function () {
+    setView($("#ordersContent"));
+});
+
+$("#placeOrders").click(function () {
+    setView($("#placeOrdersContent"));
+});
+
+
+var customers=[];
+
+$("#btnCusSave").click(function (){
+    let cusId=$("#txtCustomerID").val();
+    let cusName=$("#txtCustomerName").val();
+    let cusAddress=$("#txtCustomerAddress").val();
+    let cusSalary=$("#txtCustomerSalary").val();
+
+    var customerObject={
+        id:cusId,
+        name:cusName,
+        address:cusAddress,
+        salary:cusSalary
+
+    }
+
+    customers.push(customerObject);
+
+    loadAllCustomers();
+    loadAllCustomerId();
+    bindRowClickEvents();
+});
+
+
+function bindRowClickEvents() {
+    $("#tblCustomer>tr").click(function () {
+        let id = $(this).children(":eq(0)").text();
+        let name = $(this).children(":eq(1)").text();
+        let address = $(this).children(":eq(2)").text();
+        let salary = $(this).children(":eq(3)").text();
+
+        $('#txtCustomerID').val(id);
+        $('#txtCustomerName').val(name);
+        $('#txtCustomerAddress').val(address);
+        $('#txtCustomerSalary').val(salary);
+
+    });
+}
+
+
+function loadAllCustomers() {
+
+    $("#tblCustomer").empty();
+
+    for (var customer of customers) {
+        var row = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.salary}</td></tr>`;
+
+        $("#tblCustomer").append(row);
+    }
+}
+
+
+$("#btnCusDelete").click(function (){
+
+    let deleteId=$("#txtCustomerID").val();
+
+    let option=confirm("Do you Sure?"+deleteId);
+    if (option){
+        if (deleteCustomer(deleteId)) {
+            alert("Customer Successfully Deleted..");
+            setTextfieldValues("", "", "", "");
+        } else {
+            alert("No such customer to delete");
+        }
+    }
+});
+
+
+$("#btnCusClear").click(function (){
+    $('#txtCustomerID').val("");
+    $('#txtCustomerName').val("");
+    $('#txtCustomerAddress').val("");
+    $('#txtCustomerSalary').val("");
+});
+
+$("#btnCusUpdate").click(function () {
+
+    let customerID = $("#txtCustomerID").val();
+    let response = updateCustomer(customerID);
+    if (response) {
+        alert("Customer Updated Successfully");
+        setTextfieldValues("", "", "", "");
+    } else {
+        alert("Update Failed..!");
+
+    }
+});
+
+
+$("#txtCustomerID").on('keyup', function (event) {
+    if (event.code == "Enter") {
+        let typedId = $("#txtCustomerID").val();
+        let customer = searchCustomer(typedId);
+        if (customer != null) {
+            setTextfieldValues(customer.id, customer.name, customer.address, customer.salary);
+        } else {
+            alert("There is no cusotmer available for that " + typedId);
+            setTextfieldValues("", "", "", "");
+        }
+    }
+});
+
+function deleteCustomer(customerID) {
+    let customer = searchCustomer(customerID);
+    if (customer != null) {
+        let indexNumber = customers.indexOf(customer);
+        customers.splice(indexNumber, 1);
+        loadAllCustomers();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function setTextfieldValues(id, name, address, salary) {
+    bindRowClickEvents();
+    $("#txtCustomerID").val(id);
+    $("#txtCustomerName").val(name);
+    $("#txtCustomerAddress").val(address);
+    $("#txtCustomerSalary").val(salary);
+}
+
+
+function searchCustomer(id) {
+    for (let customer of customers) {
+        if (customer.id == id) {
+            return customer;
+        }
+    }
+    return null;
+}
+
+function updateCustomer(customerID) {
+    let customer = searchCustomer(customerID);
+    if (customer != null) {
+        customer.id = $("#txtCustomerID").val();
+        customer.name = $("#txtCustomerName").val();
+        customer.address = $("#txtCustomerAddress").val();
+        customer.salary = $("#txtCustomerSalary").val();
+        loadAllCustomers();
+        return true;
+    } else {
+        return false;
+    }
+
+}
+/*
+/!*-----------Regex-----------*!/
+const cusIDRegEx = /^(C00-)[0-9]{1,3}$/;
+const cusNameRegEx = /^[A-z ]{5,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{7,}$/;
+const cusSalaryRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+$("#txtCustomerID").focus();
+
+let customerValideaction = [];
+
+customerValideaction.push({
+    reg: cusIDRegEx,
+    field: $('#txtCustomerID'),
+    error: 'Customer ID Pattern is Wrong : ex :-C00-001'
+});
+customerValideaction.push({
+    reg: cusNameRegEx,
+    field: $('#txtCustomerName'),
+    error: 'Customer Name Pattern is Wrong : ex :-Kamal'
+});
+customerValideaction.push({
+    reg: cusAddressRegEx,
+    field: $('#txtCustomerAddress'),
+    error: 'Customer Address Pattern is Wrong : ex :-Galle'
+});
+customerValideaction.push({
+    reg: cusContactRegEx,
+    field: $('#txtCustomerSalary'),
+    error: 'Customer Contact Pattern is Wrong : ex :-5000'
+});
+
+$('#txtCustomerID,#txtCustomerName,#txtCustomerAddress,#txtCustomerAddress,#txtCustomerSalary').on('keyup', function (event) {
+    checkCusValidity();
+});
+
+$('#txtCustomerID,#txtCustomerName,#txtCustomerAddress,#txtCustomerAddress,#txtCustomerSalary').on('blur', function (event) {
+    checkCusValidity();
+});
+
+
+function checkCusValidity() {
+    let errorCount = 0;
+    for (let validation of customerValideaction) {
+        if (checkCus(validation.reg, validation.field)) {
+            textCusSuccess(validation.field, "");
+        } else {
+            errorCount = errorCount + 1;
+            setCusTextError(validation.field, validation.error);
+        }
+    }
+    setCusButtonState(errorCount);
+}
+
+function checkCus(regex, txtField) {
+    let inputValue = txtField.val();
+    return regex.test(inputValue) ? true : false;
+}
+
+function textCusSuccess(txtField, error) {
+    if (txtField.val().length <= 0) {
+        defaultCusText(txtField, "");
+    } else {
+        txtField.css('border', '2px solid green');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function setCusTextError(txtField, error) {
+    if (txtField.val().length <= 0) {
+        defaultCusText(txtField, "");
+    } else {
+        txtField.css('border', '2px solid red');
+        txtField.parent().children('span').text(error);
+    }
+}
+
+function defaultCusText(txtField, error) {
+    txtField.css("border", "1px solid #ced4da");
+    txtField.parent().children('span').text(error);
+}
+
+function setCusButtonState(value) {
+    if (value > 0) {
+        $("#save").attr('disabled', true);
+    } else {
+        $("#save").attr('disabled', false);
+    }
+}
+
+$("#clear").click(function () {
+    clearAllCusData();
+});
+
+function clearAllCusData() {
+    $('#txtCustomerID').val("");
+    $('#txtCustomerName').val("");
+    $('#txtCustomerAddress').val("");
+    $('#txtCustomerSalary').val("");
+
+    $('#id').val("");
+    $('#name').val("");
+    $('#address').val("");
+    $('#salary').val("");
+}
+
+*/
+
---
+++ assignment/js/InMemory-POS/SPA/item.js
@@ -0,0 +1,222 @@
+initiateUI();
+
+function initiateUI() {
+    clearAll();
+    $("#customerContent").css("display", "block");
+
+    setTheLastView();
+}
+
+function saveLastView(clickedID) {
+    switch (clickedID) {
+        case "customerContent":
+            localStorage.setItem("view", "CUSTOMER");
+            break;
+        case "itemContent":
+            localStorage.setItem("view", "ITEM");
+            break;
+        case "ordersContent":
+            localStorage.setItem("view", "ORDERS");
+            break;
+        case "placeOrdersContent":
+            localStorage.setItem("view", "PLACEORDERS");
+            break;
+    }
+}
+
+function setTheLastView() {
+    let view = localStorage.getItem("view");
+    switch (view) {
+        case "CUSTOMER":
+            setView($("#customerContent"));
+            break;
+        case "ITEM":
+            setView($("#itemContent"));
+            break;
+        case "ORDERS":
+            setView($("#ordersContent"));
+            break;
+        case "PLACEORDERS":
+            setView($("#placeOrdersContent"));
+            break;
+        default:
+            setView($("#customerContent"));
+    }
+}
+
+function clearAll() {
+    $("#customerContent,#itemContent,#ordersContent,#placeOrdersContent").css('display', 'none');
+}
+
+function setView(viewOb) {
+    clearAll();
+    viewOb.css("display", "block");
+    saveLastView(viewOb.get(0).id);
+    console.log(viewOb.get(0).id);
+}
+
+$("#customers").click(function () {
+    setView($("#customerContent"));
+});
+
+$("#items").click(function () {
+    setView($("#itemContent"));
+});
+
+$("#orders").click(function () {
+    setView($("#ordersContent"));
+});
+
+$("#placeOrders").click(function () {
+    setView($("#placeOrdersContent"));
+});
+
+
+var items=[];
+
+$("#btnItmSave").click(function (){
+    let itemCode=$("#txtItemCode").val();
+    let itemName=$("#txtItemName").val();
+    let itemQty=$("#txtItemQty").val();
+    let itemPrice=$("#txtItemPrice").val();
+
+    var itemObject={
+        itmCode:itemCode,
+        itmName:itemName,
+        itmQty:itemQty,
+        itmPrice:itemPrice
+
+    }
+
+    items.push(itemObject);
+
+    loadAllItems();
+    loadAllItemId();
+
+    bindRowClickEventsItems();
+});
+
+
+function bindRowClickEventsItems() {
+    $("#tblItem>tr").click(function () {
+        let itemCode = $(this).children(":eq(0)").text();
+        let itemName = $(this).children(":eq(1)").text();
+        let itemQty = $(this).children(":eq(2)").text();
+        let itemPrice = $(this).children(":eq(3)").text();
+
+        $('#txtItemCode').val(itemCode);
+        $('#txtItemName').val(itemName);
+        $('#txtItemQty').val(itemQty);
+        $('#txtItemPrice').val(itemPrice);
+
+    });
+}
+
+
+function loadAllItems() {
+
+    $("#tblItem").empty();
+
+    for (var item of items) {
+        var row = `<tr><td>${item.itmCode}</td><td>${item.itmName}</td><td>${item.itmQty}</td><td>${item.itmPrice}</td></tr>`;
+
+        $("#tblItem").append(row);
+    }
+}
+
+
+$("#btnItemDelete").click(function (){
+    /*bindRowClickEventsItems();*/
+    let deleteIds=$("#txtItemCode").val();
+
+    let option=confirm("Do you Sure?"+deleteIds);
+    if (option){
+        if (deleteItem(deleteIds)) {
+            alert("Item Successfully Deleted..");
+            setTextfieldValuesItem("", "", "", "");
+        } else {
+            alert("No such Item to delete");
+        }
+    }
+});
+
+$("#btnClearItem").click(function (){
+    $('#txtItemId').val("");
+    $('#txtItemDescription').val("");
+    $('#txtItemUnitprice').val("");
+    $('#txtItemQty').val("");
+});
+
+
+$("#btnItemUpdate").click(function () {
+    /* bindRowClickEventsItems();*/
+    let ItemId = $("#txtItemId").val();
+    let responses = updateItem(ItemId);
+    if (responses) {
+        alert("Item Updated Successfully");
+        setTextfieldValuesItem("", "", "", "");
+    } else {
+        alert("Update Failed..!");
+
+    }
+});
+
+
+$("#txtItemId").on('keyup', function (event) {
+    if (event.code == "Enter") {
+        let typedIds = $("#txtItemId").val();
+        let item = searchItem(typedIds);
+        if (item != null) {
+            setTextfieldValuesItem(item.itemId, item.descriptions, item.unitprice, item.qty);
+        } else {
+            alert("There is no cusotmer available for that " + typedIds);
+            setTextfieldValuesItem("", "", "", "");
+        }
+    }
+});
+
+function deleteItem(ItemID) {
+    let Item = searchItem(ItemID);
+    if (Item != null) {
+        let indexNumber = items.indexOf(Item);
+        items.splice(indexNumber, 1);
+        loadAllItems();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function setTextfieldValuesItem(itemId, descriptions, unitprice, qty) {
+    bindRowClickEventsItems();
+    $("#txtItemId").val(itemId);
+    $("#txtItemDescription").val(descriptions);
+    $("#txtItemUnitprice").val(unitprice);
+    $("#txtItemQty").val(qty);
+}
+
+
+function searchItem(itemID) {
+    for (let item of items) {
+        if (item.itemId == itemID) {
+            return item;
+        }
+    }
+    return null;
+}
+
+function updateItem(Items) {
+    let item = searchItem(Items);
+    if (item != null) {
+        item.itemId = $("#txtItemId").val();
+        item.descriptions = $("#txtItemDescription").val();
+        item.unitprice = $("#txtItemUnitprice").val();
+        item.qty = $("#txtItemQty").val();
+        loadAllItems();
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
